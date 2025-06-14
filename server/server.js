cec625093a706b7ad95aa4f9b8d8d33a503ce3d3
p-k-pavan <pkpavan2003@email.com>
2025-06-14 07:55:45 +0530

    Mongoose connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,26 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+
+dotenv.config();
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB, {  serverSelectionTimeoutMS: 30000 })
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to DB:", error.message);
+
+    if (error.message.includes('timed out')) {
+      console.error('Connection attempt timed out. Please check your database server and network settings.');
+    } else {
+      console.error('An unexpected error occurred:', error);
+    }
+  });
 
 app.use(express.json());
 
